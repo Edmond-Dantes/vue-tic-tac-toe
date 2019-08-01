aefeb7a52e83d487b6c54644f9129a3578008df7
Edmond <Edmond.Akpan@gmail.com>
2019-08-01 17:37:48 +0900

    basic tic tac toe game completed

--- app/components/HelloWorld.vue
+++ app/components/HelloWorld.vue
@@ -1,70 +1,131 @@
 <template>
-  <div id="app">
-    <form v-on:submit.prevent="onSubmit">
-      <label>{{ name }}</label>
-      <input v-model="message" placeholder="">
-      <p>{{ message }}</p>
-      <button>Send</button>
-    </form>
-    <button v-on:click="handleRefresh">Refresh</button>
-    <ul id="messages">
-      <li v-bind:key="item.id" v-for="item in messages">
-        <div>{{ item.author }}</div>
-        {{ item.content }}
-        <small>{{ utcMinutesFromNow(item.updated_at) }} minutes ago</small>
+  <div>
+    <h1>Tic Tac Toe</h1>
+    <h2 v-if="gameOver">{{ gameOverMessage }}</h2>
+    <h2 v-else="gameOver">Player {{ pOneTurn ? '1' : '2' }}'s turn</h2>
+    <ul>
+      <li
+        v-bind:key="box.key"
+        v-on:click.prevent="onClick(box.key)"
+        v-for="box in grid"
+        v-bind:class="{ winningTile: gameOver && winningState.includes(box.key)}"
+      >
+        {{ box.content }}
       </li>
     </ul>
+    <button
+      v-bind:disabled="!gameOver"
+      v-on:click="reset()"
+    >
+      RESET
+    </button>
   </div>
 </template>
 
 <style scoped>
-label {
-  display: block;
+ul {
+  display: flex;
+  flex-wrap: wrap;
+  width: 606px;
+  list-style: none;
+  border: 2px solid black;
+  padding: 2px;
+  text-align: center;
 }
 
-small {
-  color: gray;
+li {
+  height: 200px;
+  width: 200px;
+  border: 1px solid black;
+  line-height: 200px;
+  font-size: 150px;
+}
+
+.winningTile {
+  background-color: green;
 }
 </style>
 
 <script>
+function getDefaultGrid() {
+  return [
+    { key: 0, content: "" },
+    { key: 1, content: "" },
+    { key: 2, content: "" },
+    { key: 3, content: "" },
+    { key: 4, content: "" },
+    { key: 5, content: "" },
+    { key: 6, content: "" },
+    { key: 7, content: "" },
+    { key: 8, content: "" },
+  ];
+}
 
 export default {
   data (){
     return {
-      name: 'Edmond',
-      message: '',
-      messages: []
+      grid: [],
+      pOneTurn: true,
+      winStates: [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],
+      ],
+      gameOver: false,
+      gameOverMessage: "",
+      winningState: null,
     }
   },
-  // computed: {
-  //   utcMinutesFromNow(time) {
-  //     return Math.round((Date.now() - new Date(time)) / 60000)
-  //   }
-  // },
+  created() {
+    this.grid = getDefaultGrid();
+  },
   methods: {
-    utcMinutesFromNow(time) {
-      return Math.round((Date.now() - new Date(time)) / 60000)
+    handleGameState() {
+      const gameState = this.checkGameState();
+      if (gameState) {
+        this.gameOver = true;
+        const { message, state } = gameState;
+        this.gameOverMessage = message;
+        this.winningState = state;
+      }
     },
-    onSubmit(e) {
-      fetch('https://wagon-chat.herokuapp.com/279/messages', {
-        method: 'POST', 
-        body: JSON.stringify({
-          author: this.name,
-          content: this.message
-        })
-      })
-      .then(response => response.json())
-      .then(data => {
-        this.messages.push(data);
-      })
+    checkGameState() {
+      for (let state of this.winStates) {
+        const hasEmpty = !this.grid[state[0]].content && !this.grid[state[1]].content && !this.grid[state[2]].content;
+        const isWin = (this.grid[state[0]].content === this.grid[state[1]].content) && (this.grid[state[1]].content === this.grid[state[2]].content);
+        if (!hasEmpty && isWin) {
+          return {
+            message: this.grid[state[0]].content === 'O' ? 'Player 1 Wins!' : 'Player 2 Wins!',
+            state
+          };
+          break;
+        }
+      }
+      const boardCount = this.grid.filter(box => !!box.content).length; 
+      if (boardCount >= 9)
+        return {
+          message: 'Cat\'s Game!',
+          state: []
+        };
+      return null;
     },
-    handleRefresh(e) {
-      fetch('https://wagon-chat.herokuapp.com/279/messages')
-      .then(response => response.json())
-      .then(data => {
-        this.messages = data.messages;
-      })
+    onClick(index) {
+      if (this.gameOver || this.grid[index].content) return;
+      this.grid[index].content = this.pOneTurn ? 'O' : 'X';
+      this.pOneTurn = !this.pOneTurn;
+      this.handleGameState();
+    },
+    reset() {
+      this.grid = getDefaultGrid();
+      this.gameOver = false;
+      this.gameOverMessage = "";
+      this.winningState = null;
+      this.pOneTurn = true;
     }
   }
 }
